e9ef2a5b35d3a9473761b9da921d2e8aa566e77d
mferrara <mf8yk@virginia.edu>
2014-07-20 17:26:34 -0500

    revert for content function

--- js/main.js
+++ js/main.js
@@ -252,17 +252,3 @@ jQuery(function ($) {
   });
 });
 
-
-
-
-
-	jQuery(function ($) {	
-    // $(window).bind('load orientationchange resize', jBreadCrumbCustom);
-	  $(".content-section").bind('resize', function() {					
-				$('.equal-height').each( function() {
-							$(this).matchHeight._update();
-				});					
- 		});
-	});
-		
-		
--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -566,6 +566,15 @@
 				        easing: 'easeOutQuad',
 				        startingFrom: 2
 					});
+		});
+
+		jQuery(function ($) {	
+	    // $(window).bind('load orientationchange resize', jBreadCrumbCustom);
+		  $(".content-section").bind('resize', function() {					
+					$('.equal-height').each( function() {
+								$(this).matchHeight._update();
+					});					
+	 		});
 		});  
 	</script>
 
