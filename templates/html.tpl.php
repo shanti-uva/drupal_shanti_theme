5c807e059056770437d639e9b3eb54c715051df1
mferrara <mf8yk@virginia.edu>
2014-07-20 17:04:50 -0500

    main wrapper height 900

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -570,8 +570,8 @@
 	    // $(window).bind('load orientationchange resize', jBreadCrumbCustom);
 		  $(".content-section").bind('resize', function() {					
 					$('.equal-height').each( function() {
-								$(this).matchHeight._update().delay(500);
-					});					
+								$(this).matchHeight._update();
+					}).delay(200);					
 	 		});
 		});   
 	</script>
