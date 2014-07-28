5a77cb6268af036dc55e6fdb35fae074f8b8410a
Gerard Ketuma <me@gerardketuma.com>
2014-07-27 23:18:18 -0500

    format main template file

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -97,129 +97,127 @@
   <main class="main-wrapper container">
     <article class="main-content" role="main">
      
-    <div class="row">   
-	    <header class="col-xs-12 titlearea">
-	      <div role="banner">
-		      <h1 class="page-title"><i class="icon <?php print $subject ? "shanticon-subjects" : "shanticon-places"; ?>"></i><span><?php print $subject ? "Collections" : "Earth"; ?></span></h1>
-		      <nav class="breadwrap" role="navigation">
-		        <ol class="breadcrumb">
-		         <li><a href=""><?php print $subject ? "Subjects:" : "Places"; ?></a></li>
-		        </ol>
-		      </nav>
-        </div>
-			</header>
-    </div>
+      <div class="row">   
+  	    <header class="col-xs-12 titlearea">
+  	      <div role="banner">
+  		      <h1 class="page-title"><i class="icon <?php print $subject ? "shanticon-subjects" : "shanticon-places"; ?>"></i><span><?php print $subject ? "Collections" : "Earth"; ?></span></h1>
+  		      <nav class="breadwrap" role="navigation">
+  		        <ol class="breadcrumb">
+  		         <li><a href=""><?php print $subject ? "Subjects:" : "Places"; ?></a></li>
+  		        </ol>
+  		      </nav>
+          </div>
+  			</header>
+      </div>
     
-    <!-- Two Columns: content-resources and content-section -->
-    <div class="row row-offcanvas row-offcanvas-left">
-    
-    <!-- Column Resources  -->              
-    <aside class="content-resources col-xs-6 col-sm-3 sidebar-offcanvas equal-height">
-      <ul class="nav nav-pills nav-stacked">
-        <li class="overview active"><a href="#tab-overview" data-toggle="pill">
-        	<i class="icon shanticon-overview"></i>Overview</a>
-        </li>
-        <li class="subjects"><a href="#tab-subjects" data-toggle="pill">
-        	<i class="icon shanticon-subjects"></i>Subjects<span class="badge">5</span></a>
-        </li>
-        <li class="essays"><a href="#tab-essays" data-toggle="pill">
-        	<i class="icon shanticon-essays"></i>Essays<span class="badge">1</span></a>
-        </li>
-        <li class="places"><a href="#tab-places" data-toggle="pill">
-        	<i class="icon shanticon-places"></i>Places<span class="badge">3</span></a>
-        </li>
-        <li class="agents"><a href="#tab-agents" data-toggle="pill">
-        	<i class="icon shanticon-agents"></i>Agents<span class="badge">3</span></a>
-        </li>
-        <li class="events"><a href="#tab-events" data-toggle="pill">
-        	<i class="icon shanticon-events"></i>Events<span class="badge">3</span></a>
-        </li>
-        <li class="photos"><a href="#tab-photos" data-toggle="pill">
-        	<i class="icon shanticon-photos"></i>Photos<span class="badge">3</span></a>
-        </li>
-        <li class="audio-video"><a href="#tab-audio-video" data-toggle="pill">
-        	<i class="icon shanticon-audio-video"></i>Audio-Video<span class="badge">3</span></a>
-        </li>
-        <li class="visuals"><a href="#tab-visuals" data-toggle="pill">
-        	<i class="icon shanticon-visuals"></i>Visuals<span class="badge">3</span></a>
-        </li>
-        <li class="texts"><a href="#tab-texts" data-toggle="pill">
-        	<i class="icon shanticon-texts"></i>Texts<span class="badge">3</span></a>
-        </li>
-        <li class="maps"><a href="#tab-maps" data-toggle="pill">
-        	<i class="icon shanticon-maps"></i>Maps<span class="badge">3</span></a>
-        </li>
-        <li class="community"><a href="#tab-community" data-toggle="pill">
-        	<i class="icon shanticon-community"></i>Community<span class="badge">3</span></a>
-        </li>
-        <li class="terms"><a href="#tab-terms" data-toggle="pill">
-        	<i class="icon shanticon-terms"></i>Terms<span class="badge">3</span></a>
-        </li>
-        <li class="sources"><a href="#tab-sources" data-toggle="pill">
-        	<i class="icon shanticon-sources"></i>Sources<span class="badge">3</span></a>
-        </li>
-      </ul> 
-    </aside> 
-    
-    <!-- Column Main  -->                   
-    <section  class="content-section col-xs-12 col-sm-9 equal-height">
-	    <!-- button for responsive menu toggle -->
-	    <button type="button" class="btn btn-default btn-xs visible-xs view-resources" data-toggle="offcanvas">
-	    	<i class="fa"></i>
-	    	<span class="header">RESOURCES</span>
-	    	<span class="badge">13489</span>
-	    	<i class="icon"></i>
-	    </button>
-	    
-      <div class="tab-content">
+      <!-- Two Columns: content-resources and content-section -->
+      <div class="row row-offcanvas row-offcanvas-left">
       
-        <article class="tab-pane active" id="tab-overview">
-        </article>
-
-        <article class="tab-pane" id="tab-subjects"> 
-        </article>
-
-        <article class="tab-pane" id="tab-essays">
-				</article>
-
-				<article class="tab-pane" id="tab-places">
-        </article>
-                        
-        <article class="tab-pane" id="tab-agents">
-        </article>
-
-        <article class="tab-pane" id="tab-events">
-        </article>
-
-        <article class="tab-pane" id="tab-photos">
-        </article>
-
-        <article class="tab-pane" id="tab-audio-video">
-        </article>
-
-        <article class="tab-pane" id="tab-visuals">
-        </article>
-
-        <article class="tab-pane" id="tab-texts">
-        </article>
-
-        <article class="tab-pane" id="tab-maps">
-        </article>
-
-        <article class="tab-pane" id="tab-community">
-        </article>
-
-        <article class="tab-pane" id="tab-terms">
-        </article>
-
-        <article class="tab-pane" id="tab-sources">
-        </article>
+        <!-- Column Resources  -->              
+        <aside class="content-resources col-xs-6 col-sm-3 sidebar-offcanvas equal-height">
+          <ul class="nav nav-pills nav-stacked">
+            <li class="overview active"><a href="#tab-overview" data-toggle="pill">
+            	<i class="icon shanticon-overview"></i>Overview</a>
+            </li>
+            <li class="subjects"><a href="#tab-subjects" data-toggle="pill">
+            	<i class="icon shanticon-subjects"></i>Subjects<span class="badge">5</span></a>
+            </li>
+            <li class="essays"><a href="#tab-essays" data-toggle="pill">
+            	<i class="icon shanticon-essays"></i>Essays<span class="badge">1</span></a>
+            </li>
+            <li class="places"><a href="#tab-places" data-toggle="pill">
+            	<i class="icon shanticon-places"></i>Places<span class="badge">3</span></a>
+            </li>
+            <li class="agents"><a href="#tab-agents" data-toggle="pill">
+            	<i class="icon shanticon-agents"></i>Agents<span class="badge">3</span></a>
+            </li>
+            <li class="events"><a href="#tab-events" data-toggle="pill">
+            	<i class="icon shanticon-events"></i>Events<span class="badge">3</span></a>
+            </li>
+            <li class="photos"><a href="#tab-photos" data-toggle="pill">
+            	<i class="icon shanticon-photos"></i>Photos<span class="badge">3</span></a>
+            </li>
+            <li class="audio-video"><a href="#tab-audio-video" data-toggle="pill">
+            	<i class="icon shanticon-audio-video"></i>Audio-Video<span class="badge">3</span></a>
+            </li>
+            <li class="visuals"><a href="#tab-visuals" data-toggle="pill">
+            	<i class="icon shanticon-visuals"></i>Visuals<span class="badge">3</span></a>
+            </li>
+            <li class="texts"><a href="#tab-texts" data-toggle="pill">
+            	<i class="icon shanticon-texts"></i>Texts<span class="badge">3</span></a>
+            </li>
+            <li class="maps"><a href="#tab-maps" data-toggle="pill">
+            	<i class="icon shanticon-maps"></i>Maps<span class="badge">3</span></a>
+            </li>
+            <li class="community"><a href="#tab-community" data-toggle="pill">
+            	<i class="icon shanticon-community"></i>Community<span class="badge">3</span></a>
+            </li>
+            <li class="terms"><a href="#tab-terms" data-toggle="pill">
+            	<i class="icon shanticon-terms"></i>Terms<span class="badge">3</span></a>
+            </li>
+            <li class="sources"><a href="#tab-sources" data-toggle="pill">
+            	<i class="icon shanticon-sources"></i>Sources<span class="badge">3</span></a>
+            </li>
+          </ul> 
+        </aside> 
       
-      </div><!-- END tab-content -->
-    </section><!-- END content-section -->    
+        <!-- Column Main  -->                   
+        <section  class="content-section col-xs-12 col-sm-9 equal-height">
+    	    <!-- button for responsive menu toggle -->
+    	    <button type="button" class="btn btn-default btn-xs visible-xs view-resources" data-toggle="offcanvas">
+    	    	<i class="fa"></i>
+    	    	<span class="header">RESOURCES</span>
+    	    	<span class="badge">13489</span>
+    	    	<i class="icon"></i>
+    	    </button>
+    	    
+          <div class="tab-content">
+          
+            <article class="tab-pane active" id="tab-overview">
+            </article>
 
-                  
-    </div><!-- END row -->    
+            <article class="tab-pane" id="tab-subjects"> 
+            </article>
+
+            <article class="tab-pane" id="tab-essays">
+    				</article>
+
+    				<article class="tab-pane" id="tab-places">
+            </article>
+                            
+            <article class="tab-pane" id="tab-agents">
+            </article>
+
+            <article class="tab-pane" id="tab-events">
+            </article>
+
+            <article class="tab-pane" id="tab-photos">
+            </article>
+
+            <article class="tab-pane" id="tab-audio-video">
+            </article>
+
+            <article class="tab-pane" id="tab-visuals">
+            </article>
+
+            <article class="tab-pane" id="tab-texts">
+            </article>
+
+            <article class="tab-pane" id="tab-maps">
+            </article>
+
+            <article class="tab-pane" id="tab-community">
+            </article>
+
+            <article class="tab-pane" id="tab-terms">
+            </article>
+
+            <article class="tab-pane" id="tab-sources">
+            </article>
+          
+          </div><!-- END tab-content -->
+        </section><!-- END content-section -->                
+      </div><!-- END row -->    
     </article><!-- END main-content -->
  
  
